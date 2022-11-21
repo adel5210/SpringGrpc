21ec29a46d779a65eae64562ceefac6d240ab75c
adel <adel.albediwy@gmail.com>
2022-11-21 19:08:40 +0300

    Initial Commit

---
+++ src/test/java/com/adel/springgrpc/SpringGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adel.springgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
